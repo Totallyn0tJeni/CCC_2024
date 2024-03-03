47c7ac313762cffca0db7d1868cf8bb5d49d9d54
Jenisha Patel <161256647+Totallyn0tJeni@users.noreply.github.com>
2024-03-03 18:07:55 -0500

    Add files via upload

---
+++ Problem#1 Conveyor Belt Sushi.py
@@ -0,0 +1,11 @@
+redPlate = 3
+greenPlate = 4
+bluePlate = 5
+
+R = int(input()) #Input Number Of Red Plates
+G = int(input()) #Input Number Of Green Plates
+B = int(input()) #Input Number Of Blue Plates
+
+C = (redPlate*R) + (greenPlate*G) + (bluePlate*B)
+
+print (C)
